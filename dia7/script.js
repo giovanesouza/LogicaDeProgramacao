c865e5117843081a895f1c39d8d9564aa4ab1428
giovanesouza <developergiovanesouza@gmail.com>
2022-08-16 14:54:08 -0300

    Desenvolvimento da estrutura da calculadora (HTML) e estilização (CSS) + Criação das funções para as operações matemáticas básicas - Adição, subtração, multiplicação e divisão.

---
+++ dia7/script.js
@@ -0,0 +1,32 @@
+
+
+let n1 = 0;
+let n2 = 0;
+let resultado = 0;
+
+
+// OPERAÇÕES MATEMÁTICAS
+
+function adicao(n1, n2) {
+    return n1 + n2
+}
+
+function subtracao(n1, n2) {
+    return n1 - n2
+}
+
+function multiplicacao(n1, n2) {
+    return n1 * n2
+}
+
+function divisao(n1, n2) {
+    return n1 / n2
+}
+
+
+
+
+console.log(adicao(5, 5));
+console.log(subtracao(5, 5));
+console.log(multiplicacao(5, 5));
+console.log(divisao(5, 5));
